e70b81d955f466664ead3b9ca0a1ac97aee84eeb
sansan <yeztree98@gmail.com>
2025-08-31 19:32:45 +0800

    app 打包 组件不会加载进来处理

--- main.js
+++ main.js
@@ -13,8 +13,34 @@ app.$mount()
 
 // #ifdef VUE3
 import { createSSRApp } from 'vue'
+
+import UButton from '@climblee/uv-ui/components/uv-button/uv-button.vue'
+import UInput from '@climblee/uv-ui/components/uv-input/uv-input.vue'
+import UStatusBar from '@climblee/uv-ui/components/uv-status-bar/uv-status-bar.vue'
+import UTags from '@climblee/uv-ui/components/uv-tags/uv-tags.vue'
+import UAvatar from '@climblee/uv-ui/components/uv-avatar/uv-avatar.vue'
+import UText from '@climblee/uv-ui/components/uv-text/uv-text.vue'
+import UIcon from '@climblee/uv-ui/components/uv-icon/uv-icon.vue'
+import UEmpty from '@climblee/uv-ui/components/uv-empty/uv-empty.vue'
+import ULine from '@climblee/uv-ui/components/uv-line/uv-line.vue'
+import UList from '@climblee/uv-ui/components/uv-list/uv-list.vue'
+import UListItem from '@climblee/uv-ui/components/uv-list-item/uv-list-item.vue'
+import UDivider from '@climblee/uv-ui/components/uv-divider/uv-divider.vue'
 export function createApp() {
+	
   const app = createSSRApp(App)
+  app.component(UButton.name, UButton)
+  app.component(UInput.name, UInput)
+  app.component(UStatusBar.name, UStatusBar)
+  app.component(UTags.name, UTags)
+  app.component(UAvatar.name, UAvatar)
+  app.component(UText.name, UText)
+   app.component(UIcon.name, UIcon)
+   app.component(UEmpty.name, UEmpty)
+   app.component(UList.name, UList)
+   app.component(UListItem.name, UListItem)
+    app.component(ULine.name, ULine)
+	 app.component(UDivider.name, UDivider)
   return {
     app
   }
